83b01c1225c9cb3e8e169d2c43e37da92f24f6df
96bp19 <lanthoo170@gmail.com>
2020-01-13 15:01:30 +0545

    Project Initialization

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private LevelGenerator levelGeneratorPrefab;
+
+    private LevelGenerator _levelGeneratorInstance;
+
+    [SerializeField] private int noOfPlatformsToGenerate = 10;
+    
+    void Start()
+    {
+        BeginGame();
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    void BeginGame()
+    {
+        _levelGeneratorInstance = Instantiate(levelGeneratorPrefab);
+        _levelGeneratorInstance.GenerateLevel(noOfPlatformsToGenerate);
+    }
+
+    void RestartGame()
+    {
+        Destroy(_levelGeneratorInstance.gameObject);
+        BeginGame();
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] PlatformPrefabs;
+
+    [SerializeField]
+    private float platformWidth=10f;
+
+    private List<Transform> spawnedPlatforms = new List<Transform>();
+
+    // generates specified no of platforms 
+    public void GenerateLevel(int noOfPlatformsToGenerate)
+    {
+        RemovePreviousPlatforms();
+        Vector2 SpawnedObjPos = Vector2.zero;
+        int randomIndex = 0;
+        for (int i = 0; i < noOfPlatformsToGenerate; i++)
+        {
+            randomIndex = Random.Range(0, PlatformPrefabs.Length);
+            Transform platform = Instantiate(PlatformPrefabs[randomIndex]).transform;
+            platform.SetParent(transform);
+            SpawnedObjPos.x = i * platformWidth;
+            platform.localPosition = SpawnedObjPos;
+            spawnedPlatforms.Add(platform);
+                  
+        }
+
+    }
+
+    // removes any spawned platforms from the level
+    public void RemovePreviousPlatforms()
+    {
+        foreach (var item in spawnedPlatforms)
+        {
+            Destroy(item.gameObject);
+        }
+        spawnedPlatforms.Clear();
+    }
+}
